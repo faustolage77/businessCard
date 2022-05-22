bf3f15dc4844f70bc1b910f5dfb87604ecaa494d
Fausto Lage <100029945+faustolage77@users.noreply.github.com>
2022-05-22 18:30:29 -0300

    Add files via upload

--- comments.js
+++ comments.js
@@ -4,3 +4,4 @@
 // git commit -m "add whatever" => commita as mudancas 
 // git publish input authenticator password => ficar atento ao versionamento, nao vai conseguir publicar mudancas sob o nome de uma versao ja existente
 // usefull commands: clear, git status, 
+// ficar de olho na pastab bin
--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const owner = {Name: "Fausto Lage",
 Title: 'Advogado - Programador em formacao',
 GitHub: 'faustolage77',
 url: 'https://www.linkedin.com/in/fausto-lage-a1890a149/ ',
-Lang: 'JavaScript, Python'}
+Lang: 'JavaScript, Python'};
 
 console.log(owner);
