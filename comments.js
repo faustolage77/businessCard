85422e78a7188d546502d26ab86e1a0b52525e6d
Fausto Lage <100029945+faustolage77@users.noreply.github.com>
2022-05-22 18:01:42 -0300

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,6 @@
+// desenvolver esta info comando npx faustolage 
+// sempre lembrar de criar PASTAS SEPARADAS PARA CADA PROJETO
+// git add . => adiciona os elementos 
+// git commit -m "add whatever" => commita as mudancas 
+// git publish input authenticator password => ficar atento ao versionamento, nao vai conseguir publicar mudancas sob o nome de uma versao ja existente
+// usefull commands: clear, git status, 
---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const owner = {Name: "Fausto Lage",
+Title: 'Advogado - Programador em formacao',
+GitHub: 'faustolage77',
+url: 'https://www.linkedin.com/in/fausto-lage-a1890a149/ ',
+Lang: 'JavaScript, Python'}
+
+console.log(owner);
